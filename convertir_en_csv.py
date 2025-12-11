d4f560306e6e8fe7fddf954c92ea727ab2856df5
id2733 <clemence.pinsard@eleve.ensai.fr>
2025-12-11 14:08:25 +0000

    modif2

--- convertir_en_csv.py
+++ convertir_en_csv.py
@@ -1,28 +1,31 @@
 import ezc3d
 import csv
 
+# Dictionnaire : nom du CSV → fichier C3D à lire
+fichiers = {
+    "Chrono250": "data/Chrono250_c3d.c3d",
+    "Chrono300": "data/Chrono300_c3d.c3d",
+    "Chrono350": "data/Chrono350_c3d.c3d",
+    "Route250":  "data/Route250_c3d.c3d",
+    "Route300":  "data/Route300_c3d.c3d",
+    "Route350":  "data/Route350_c3d.c3d",
+}
 
-c3d_Chrono250 = ezc3d.c3d("data/Chrono250_c3d.c3d")
-c3d_Chrono300 = ezc3d.c3d("data/Chrono300_c3d.c3d")
-c3d_Chrono500 = ezc3d.c3d("data/Chrono350_c3d.c3d")
-c3d_Route250 = ezc3d.c3d("data/Route250_c3d.c3d")
-c3d_Route300 = ezc3d.c3d("data/Route300_c3d.c3d")
-c3d_Route500 = ezc3d.c3d("data/Route350_c3d.c3d")
+for nom, chemin in fichiers.items():
 
-liste_fichiers = [c3d_Chrono250, c3d_Chrono300, c3d_Chrono500, c3d_Route250, c3d_Route300,
-                  c3d_Route500]
-
-for c3d in liste_fichiers:
-    points = c3d['data']['points']  # Données des marqueurs
+    c3d = ezc3d.c3d(chemin)
+    points = c3d['data']['points']
     marker_names = c3d['parameters']['POINT']['LABELS']['value']
     num_markers = points.shape[1]
     num_frames = points.shape[2]
 
-    with open("markers.csv", "w", newline="") as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(["Frame", "Marker", "Marker_Name", "X", "Y", "Z", "Valid"])  # En-têtes
+    # nom du CSV en fonction du fichier C3D
+    csv_filename = f"{nom}.csv"
 
-        # Boucle sur toutes les trames et tous les marqueurs
+    with open(csv_filename, "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["Frame", "Marker", "Marker_Name", "X", "Y", "Z", "Valid"])
+
         for frame in range(num_frames):
             for marker in range(num_markers):
                 x = points[0, marker, frame]
@@ -30,4 +33,7 @@ for c3d in liste_fichiers:
                 z = points[2, marker, frame]
                 valid = points[3, marker, frame]
                 name = marker_names[marker]
+
                 csv_writer.writerow([frame + 1, marker + 1, name, x, y, z, valid])
+
+    print(f"✔ CSV généré : {csv_filename}")
