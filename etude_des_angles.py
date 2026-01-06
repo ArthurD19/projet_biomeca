619a078021f0af4dd65e7b70f7b2ced3dddda922
Arthur Deletang <arthur.deletang@eleve.ensai.fr>
2026-01-06 09:42:38 +0100

    graph

--- etude_des_angles.py
+++ etude_des_angles.py
@@ -110,3 +110,30 @@ for articulation in df_angles_Route350["Articulation"].unique():
     })
 df_results_Route350 = pd.DataFrame(results_Route350)
 print(df_results_Route350.to_string(index=False))
+
+import matplotlib.pyplot as plt
+
+# Regroupement des données pour le graphisme (exemple pour le RoM)
+# Vous pouvez créer un DataFrame global regroupant vos df_results_...
+all_results = pd.concat([
+    df_results_Chrono250.assign(Pos='Chrono', P=250),
+    df_results_Chrono300.assign(Pos='Chrono', P=300),
+    df_results_Chrono350.assign(Pos='Chrono', P=350),
+    df_results_Route250.assign(Pos='Route', P=250),
+    df_results_Route300.assign(Pos='Route', P=300),
+    df_results_Route350.assign(Pos='Route', P=350)
+])
+
+# Graphique 1 : Comparaison du RoM Moyen
+rom_avg = all_results.groupby(['Pos', 'Articulation'])['RoM'].mean().unstack(level=0)
+rom_avg.plot(kind='bar', figsize=(10, 6))
+plt.title('Comparaison du RoM Moyen : Route vs Chrono')
+plt.ylabel('Degrés')
+plt.savefig('rom_comparison.png')
+
+# Graphique 2 : Evolution de la Hanche Minimum
+hanche_min = all_results[all_results['Articulation'].str.contains('Hanche')].pivot_table(index='P', columns=['Pos', 'Articulation'], values='Minimum')
+hanche_min.plot(marker='o', figsize=(10, 6))
+plt.title('Angle de Hanche Minimum vs Puissance')
+plt.ylabel('Degrés')
+plt.savefig('hanche_min_power.png')
